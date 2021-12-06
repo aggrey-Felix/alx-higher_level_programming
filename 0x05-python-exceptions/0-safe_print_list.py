2b130eb7d50ed08f2c52c1151c85afeb2e174ff6
felix abongo <aggreyfelix@yahoo.com>
2021-12-06 00:32:29 -0800

    first commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        outcome = a / b
+    except ZeroDivisionError:
+        outcome = None
+    finally:
+        print("Inside outcome: {}".format(outcome))
+    return (outcome)
