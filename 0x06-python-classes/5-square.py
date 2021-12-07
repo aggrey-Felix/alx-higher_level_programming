2459a0514170a44e0783bc5d6e8362603e54f1b1
felix abongo <aggreyfelix@yahoo.com>
2021-12-07 04:09:23 -0800

    square with the character #

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,42 +1,59 @@
 #!/usr/bin/python3
+"""Defines a class Square"""
 
 
 class Square:
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of the square
+    """
     def __init__(self, size=0):
-        if type(size) is not int:
-            raise TypeError('size must be an integer')
-        else:
-            if size < 0:
-                raise ValueError('size must be >= 0')
-            else:
-                self.__size = size
+        """initializes the square
+        Args:
+            size (int): size of a side of the square
+        Returns:
+            None
+        """
+        self.size = size
 
     def area(self):
-        return (self.__size) ** 2
+        """calculates the square's area
+        Returns:
+            The area of the square
+        """
+        return (self.__size) * (self.__size)
 
     @property
     def size(self):
-        return (self.__size)
+        """getter of __size
+        Returns:
+            The size of the square
+        """
+        return self.__size
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError('size must be an integer')
+        """setter of __size
+        Args:
+            value (int): size of a side of the square
+        Returns:
+            None
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
         else:
             if value < 0:
-                raise ValueError('size must be >= 0')
+                raise ValueError("size must be >= 0")
             else:
                 self.__size = value
 
     def my_print(self):
+        """prints the square
+        Returns:
+            None
+        """
         if self.__size == 0:
             print()
             return
         for i in range(self.__size):
-            print(''.join(['#' for j in range(self.__size)]))
-
-
-obj = Square()
-obj.area()
-obj.size()
-obj.my_print()
+            print("".join(["#" for j in range(self.__size)]))
