fc5c1f8bd63d57fa9cde44d8e4495cc25f88df14
Felix abongo <aggreyfelix68@gmail.com>
2022-03-01 10:23:44 +0000

    script that concats 2 files

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/*
+*Write a script that concats 2 files
+*The first argument is the file path of the first source file
+*The second argument is the file path of the second source file
+*The third argument is the file path of the destination
+*/
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
+const fs = require('fs');
+const textA = fs.readFileSync(fileA, 'utf8');
+const textB = fs.readFileSync(fileB, 'utf8');
+fs.writeFileSync(fileC, textA + textB);
