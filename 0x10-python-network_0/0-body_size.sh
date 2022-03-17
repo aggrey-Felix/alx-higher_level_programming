207d37f13d831b1c77c0f45fe01f831c4eab8791
felix abongo <aggreyfelix@yahoo.com>
2022-03-17 04:00:00 -0700

    first commit

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/python3
-#Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sI "$1" | grep 'Content-Length:' | cut -d' ' -f2
+#!/bin/bash
+#script takes in a url, ends a request to that URL displays the size of the response body
+curl -sI "$1" | grep -i Content-Length | cut -d ' ' -f2
