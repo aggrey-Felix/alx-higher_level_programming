88a2de7e599a40db37e047aa321f2b0e8361f72f
felix abongo <aggreyfelix@yahoo.com>
2022-03-18 02:34:52 -0700

    script fixed

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-"""
-Python script that fetches https://alx-intranet.hbtn.io/status
--You must use the package requests
--You are not allow to import packages other than requests
--The body of the response must be display like the following example
-(tabulation before -)
-"""
+"""fetches https://intranet.hbtn.io/status."""
+import requests
+
 
 if __name__ == "__main__":
-    import requests
     r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
