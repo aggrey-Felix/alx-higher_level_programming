9a9eeaef38434de02182d4e36e0eb91cd040b3be
felix abongo <aggreyfelix@yahoo.com>
2022-03-18 02:36:56 -0700

    code fixed on script

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """
-script that takes in a URL and an email, sends a POST request to the passed URL
-with the email as a parameter, and displays the body of the response (decoded
-in utf-8)
+takes in a URL and an email, sends a POST request to the passed URL with the
+email as a parameter, and displays the body of the response
 """
-
 if __name__ == "__main__":
+    import urllib.parse as parse
     import urllib.request as request
     from sys import argv
     values = {'email': argv[2]}
